917cf3d874fec4fdfaa40a48a194814e4ccd8fb7
Austin White <austingym@gmail.com>
2014-05-20 12:49:41 -0700

    Updated redis database config

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -85,10 +85,10 @@ return array(
 
 	'redis' => array(
 
-		'cluster' => true,
+		'cluster' => false,
 
 		'default' => array(
-			'host'     => '127.0.0.1',
+			'host'     => 'tunnel.pagodabox.com',
 			'port'     => 6379,
 			'database' => 0,
 		),
