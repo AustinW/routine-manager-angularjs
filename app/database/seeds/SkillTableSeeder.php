036c5196849bffea037cf5c71e7384c2c4fd8c3a
Austin White <austingym@gmail.com>
2014-05-20 12:06:56 -0700

    Fixed undefined variable $skills

--- app/database/seeds/SkillTableSeeder.php
+++ app/database/seeds/SkillTableSeeder.php
@@ -1163,7 +1163,7 @@ class SkillTableSeeder extends \Illuminate\Database\Seeder
 		];
 
 		if (LRedis::connection()) {
-			LRedis::pipeline(function($redisPipe) {
+			LRedis::pipeline(function($redisPipe) use($skills) {
 				$redisPipe->hdel('skills:name', '*');
 
 				$fields = array_keys($skills[0]);
